d6d49e382186e42f17341714e548f1c2b00b8544
Martin C <martin.couture@uqtr.ca>
2020-09-10 20:12:22 -0400

    Update Bissextile.py
    Correction

--- Exemples cours 1/Bissextile.py
+++ Exemples cours 1/Bissextile.py
@@ -2,21 +2,25 @@
 # Programme testant si une année, saisie par l'utilisateur,
 # est bissextile ou non
 
-annee = input("Saisissez une année : ") # On attend que l'utilisateur saisisse l'année qu'il désire tester
+annee = input("Entrer une année : ") # On attend que l'utilisateur saisisse l'année qu'il désire tester
 annee = int(annee) # Risque d'erreur si l'utilisateur n'a pas saisi un nombre
-bissextile = False # On crée un booléen qui vaut faux
-if annee % 400 == 0:
-    bissextile = True
-elif annee % 100 == 0:
-    bissextile = False
-elif annee % 4 == 0:
-    bissextile = True
-else:
-    bissextile = False
 
-if bissextile: # Si l'année est bissextile
-    print("L'année saisie est bissextile.")
+bissextile = False # On crée un booléen qui vaut faux
+
+if annee % 4 != 0:
+    bissextile = False
+elif annee % 4 ==0:
+    if annee % 100 != 0:
+        bissextile = True
+    elif annee %100 ==0:
+        if annee % 400 != 0:
+            bissextile = False
+        else:
+            bissextile = True
+
+if bissextile:
+    print("L'année entrée est bissextile")
 else:
-    print("L'année saisie n'est pas bissextile.")
+    print("L'année entrée n'est pas bissextile")
 
 
