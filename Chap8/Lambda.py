522abf7e191927e97f85d78d95964ff5d808f8dd
Martin C <martin.couture@uqtr.ca>
2021-10-12 21:42:29 -0400

    Delete Chap8 directory

--- Chap8/Lambda.py
+++
@@ -1,8 +0,0 @@
-def exp2(nombre):
-    return nombre**2
-#La fonction ci-dessus équivaut à l'expression Lambda ci-dessous.
-exposant2 = lambda nombre : nombre**2
-
-print(exposant2(8))
-
-
--- Chap8/Map_Lambda.py
+++
@@ -1,9 +0,0 @@
-
-listeNombre = [1,2,3,4]
-lstResultat = map(lambda nombre: nombre**2, listeNombre)
-
-for resultat in lstResultat:
-    print(resultat)
-
-
-    
